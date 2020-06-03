76dbcd5e1f9abcc3b87df0c097b28023c3afa5d1
Madeline Greene <greenem6@students.rowan.edu>
2020-06-02 21:44:43 -0400

    Working with numbers code

---
+++ 01-working-with-numbers.py
@@ -0,0 +1,16 @@
+Python 3.8.3 (v3.8.3:6f8c8320e9, May 13 2020, 16:29:34) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print(5/7)
+0.7142857142857143
+>>> print(5*7)
+35
+>>> a = 5
+>>> print (a)
+5
+>>> b = 5
+>>> c = -33
+>>> d = b + c
+>>> print(d)
+-28
+>>> 
